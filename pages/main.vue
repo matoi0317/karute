28c5015657a77081d84432aee7638df24f0790c1
matoi0317 <matoi.shibanuma@gmail.com>
2021-06-12 14:30:28 +0900

    m1

---
+++ pages/main.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="contents">
+            <div class="view-karute"></div>
+            <div class="info"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.contents{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+.view-karute{
+    width: 80%;
+    height: 250px;
+    background-color: red;
+}
+
+.info{
+    width: 80%;
+    height: 250px;
+    background-color: blue;  
+}
+</style>
