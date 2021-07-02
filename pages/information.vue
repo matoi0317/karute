a49850bdddd79fbfee7e26859f2729c1c281077f
matoi0317 <matoi.shibanuma@gmail.com>
2021-07-02 17:57:10 +0900

    aaa

--- pages/information.vue
+++ pages/information.vue
@@ -3,8 +3,19 @@
         <div class="header">
             <p class="header-title">個人情報</p>
         </div>
-        <div class="profile-icon"></div>
-        <hr>
+        <div class="profile">
+            <img src="./b.png" class="profile-icon">
+            <div class="username">UserName</div>
+        </div>
+        <div class="underline"></div>
+        <div class="cp_iptxt">
+	        <input type="text" placeholder="お名前">
+	        <i class="fa fa-user fa-lg fa-fw" aria-hidden="true"></i>
+        </div>
+        <div class="cp_iptxt">
+	        <input type="text" placeholder="E-Mail">
+	        <i class="fa fa-envelope fa-lg fa-fw" aria-hidden="true"></i>
+        </div>
     </div>
 </template>
 
@@ -13,11 +24,6 @@ export default {}
 </script>
 
 <style>
-hr{
-    margin: 0 auto;
-    width:90%;
-    border-top:1px solid#f1f1f1;
-}
 .header{
     display: table;
     width:100%;
@@ -33,8 +39,64 @@ hr{
     font-weight: bold;
 }
 
-.profile-icon{
+.profile{
+    display: flex;
     width:100%;
-    height:110px;
+    height:130px;
+}
+
+.underline{
+    border: solid 1px #f2f2f2;
+    width: 80%;
+    margin: 0 auto;
+}
+
+.profile-icon{
+    height: 100%;
+}
+
+.username{
+    font-size: 40px;
+    font-weight: 200;
+    padding-top: 11%;
+}
+
+.cp_iptxt {
+	position: relative;
+	width: 50%;
+	margin: 40px 3%;
+}
+.cp_iptxt input[type='text'] {
+	font: 15px/24px sans-serif;
+	box-sizing: border-box;
+	width: 100%;
+	padding: 0.3em;
+	padding-left: 40px;
+	letter-spacing: 1px;
+	border: 0;
+}
+.cp_iptxt input[type='text']:focus {
+	outline: none;
+}
+.cp_iptxt input[type='text']:focus::after {
+	outline: none;
+}
+.cp_iptxt i {
+	position: absolute;
+	top: 0;
+	left: 0;
+	padding: 9px 5px;
+	transition: 0.3s;
+	color: #aaaaaa;
+}
+.cp_iptxt::after {
+	display: block;
+	width: 100%;
+	height: 4px;
+	margin-top: -1px;
+	content: '';
+	border-width: 0 1px 1px 1px;
+	border-style: solid;
+	border-color: #aaaaaa;
 }
 </style>
